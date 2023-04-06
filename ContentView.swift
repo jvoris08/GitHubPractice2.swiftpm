a1f91cd16a531395a23edd652bb770436ae408ad
Jake <jvoris6444@stu.d214.org>
2023-04-06 14:19:46 -0500

    Made NavigationView and Link to Nounview

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            NavigationLink {
+                NounView()
+            } label: {
+                "Noun"
+            }
         }
     }
 }
