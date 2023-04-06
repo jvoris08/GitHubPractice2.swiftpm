2d177d3a51ac68f7b2b2f5cd1c72c12d055b6a63
Vasil <vpopov6335@stu.d214.org>
2023-04-06 14:19:43 -0500

    Added NounView

---
+++ NounView.swift
@@ -0,0 +1,20 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by Vasil Popov on 4/6/23.
+//
+
+import SwiftUI
+
+struct NounView: View {
+    var body: some View {
+            NavigationLink {
+                VerbView()
+            } label: {
+                "Verb"
+            }
+
+    }
+}
+
